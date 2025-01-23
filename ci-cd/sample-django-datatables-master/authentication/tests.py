02cc080d0a14a9f22ea803284741f81b6bd16bc0
Mehdi Benouhoud <mehdi.benouhoud@polymtl.ca>
2025-01-23 18:55:02 -0500

    huge commit

---
+++ ci-cd/sample-django-datatables-master/authentication/tests.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.test import TestCase
+
+# Create your tests here.
+
+class DummyTestForExam(TestCase):
+    def test_case_1(self):
+        self.assertEqual(2, 1 + 1)
